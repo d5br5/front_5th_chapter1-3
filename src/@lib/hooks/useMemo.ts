e0535b5066c52d7a3b5617e6fe81cdfdf89cb9df
Doh Kim <ehgud456456@naver.com>
2025-04-07 20:02:57 +0900

    fix: useMemo 의존성 비교 로직 정상화

--- src/@lib/hooks/useMemo.ts
+++ src/@lib/hooks/useMemo.ts
@@ -12,10 +12,8 @@ export function useMemo<T>(
   const prevResultRef = useRef<T | undefined>(undefined);
 
   // 2. 현재 의존성과 이전 의존성 비교
-  const isDepsSame = _equals(_deps, prevDepsRef.current);
-
   // 3. 의존성이 변경된 경우 factory 함수 실행 및 결과 저장
-  if (!isDepsSame) {
+  if (!prevDepsRef.current || !_equals(_deps, prevDepsRef.current)) {
     prevDepsRef.current = _deps;
     prevResultRef.current = factory();
   }
