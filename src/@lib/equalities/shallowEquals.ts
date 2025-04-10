c5bd9ee3478712f26695bd684fdb83640e658720
Doh Kim <ehgud456456@naver.com>
2025-04-10 18:35:59 +0900

    fix: 일치 비교를 `Object.is`로 교체

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -3,13 +3,17 @@ const isObj = (obj: unknown): obj is Record<string, unknown> => {
 };
 
 export function shallowEquals<T>(objA: T, objB: T): boolean {
-  if (objA === objB) return true;
+  // 원시타입 비교 (둘 다 원시타입이면 여기서 걸러짐)
+  if (Object.is(objA, objB)) return true;
 
+  // 둘 다 원시타입일때 비교는 위에서 했으므로, 하나만 원시타입이면 false
   if (!isObj(objA) || !isObj(objB)) return false;
 
+  // 둘다 객체인 상태. key 개수 비교
   const keysA = Object.keys(objA);
   const keysB = Object.keys(objB);
   if (keysA.length !== keysB.length) return false;
 
+  // key 개수는 같으므로, 모든 key값에 대한 value 비교
   return keysA.every((key) => objA[key] === objB[key]);
 }
